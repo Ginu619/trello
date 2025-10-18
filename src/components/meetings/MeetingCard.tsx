fbc2f1e2e865dbdfd5ee3da3c01222bfee6989c1
bony binoy <binoybony90@gmail.com>
2025-10-18 04:19:17 +0000

    meeting title should show the full length of card

--- src/components/meetings/MeetingCard.tsx
+++ src/components/meetings/MeetingCard.tsx
@@ -126,8 +126,8 @@ export function MeetingCard({ meeting, team, variant = 'default', onMeetingUpdat
             <Card className="flex flex-col bg-card/80 hover:bg-card/90 transition-colors cursor-pointer" onClick={openDetails}>
                 <CardHeader>
                     <div className="flex justify-between items-start">
-                        <CardTitle className="text-lg">{meeting.title}</CardTitle>
-                        <div className="flex items-center -mr-2">
+                        <CardTitle className="text-lg flex-1 mr-4">{meeting.title}</CardTitle>
+                        <div className="flex items-center -mr-2 flex-shrink-0">
                             <MeetingStatusBadge status={meeting.status} />
                            {actionButton}
                         </div>
